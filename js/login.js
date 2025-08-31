84dde22e8ee02f67ef2d3e3de0674e07f776637c
DOMKA <software.domka@gmail.com>
2025-08-31 18:49:54 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,9 +1,16 @@
-// js/login.js
 import { loginUser } from "./auth.js";
 
-document.getElementById("login-form").addEventListener("submit", async (e) => {
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
   e.preventDefault();
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  await loginUser(email, password);
+  const companyId = document.getElementById("companyId").value;
+  const errorMsg = document.getElementById("errorMsg");
+
+  try {
+    await loginUser(email, password, companyId);
+    window.location.href = "dashboard.html";
+  } catch (err) {
+    errorMsg.textContent = err.message;
+  }
 });
