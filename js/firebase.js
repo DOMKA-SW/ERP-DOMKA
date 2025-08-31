56f8f06072b50d7843eb23819f6e01e700532bb0
DOMKA <software.domka@gmail.com>
2025-08-31 18:44:44 -0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,9 +2,9 @@
 // Configuración de Firebase DOMKA ERP
 
 // Importar SDK (v9 modular)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 
 // ⚠️ config real de Firebase
 const firebaseConfig = {
